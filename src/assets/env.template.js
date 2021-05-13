22f5132db2297ba7dbf62df6665aeb5c33bb5898
rcontreras <ramiro.contreras@mercadolibre.com.co>
2021-05-12 20:57:31 -0500

    add container config

---
+++ src/assets/env.template.js
@@ -0,0 +1,6 @@
+(function (window) {
+  window.env = window.env || {};
+
+  // Environment variables
+  window["env"]["api_url"] = "${API_URL}";
+})(this);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   api_url: 'https://conduit.productionready.io/api'
+  //api_url: window["env"]["apiUrl"] || "default",
 };
