1ee377f55a2e662a2ab7a24c0847a50a264d6451
rcontreras <ramiro.contreras@mercadolibre.com.co>
2021-05-13 20:55:47 -0500

    imagenes home y data

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["api_url"] = "localhost:8080/111";
+    window["env"]["api_url"] = "https://conduit.productionready.io/api";
     window["env"]["authorization"] = "";
   })(this);                                                
